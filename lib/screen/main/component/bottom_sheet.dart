a4552873f1792eda7f5222fe2271bc223e081758
Ali Heidari <a.heidari@cpol.co>
2023-12-03 21:44:22 +0330

    02/09/12

---
+++ lib/screen/main/component/bottom_sheet.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetWidget extends StatelessWidget {
+  const BottomSheetWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size=MediaQuery.of(context).size;
+    return  Container(
+
+    );
+  }
+}
--- lib/screen/main/main_page.dart
+++ lib/screen/main/main_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
-
+import 'package:mohaseb/screen/main/component/bottom_sheet.dart';
 
 @RoutePage(name: "main")
 class MainPage extends StatelessWidget {
@@ -8,6 +8,13 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Align(alignment: Alignment.bottomCenter,child: BottomSheetWidget(),)
+        ],
+      ),
+    );
   }
 }
